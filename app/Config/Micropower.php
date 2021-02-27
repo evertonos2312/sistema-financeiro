5b0f15ca3adb8ef50b9c8fe4f6d980719f40d70b
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2021-02-27 00:43:37 -0300

    Config Inicial

--- app/Config/Micropower.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Config;
-
-class Micropower extends \CodeIgniter\Config\BaseConfig
-{
-	public $login = '999999';
-	public $password = 'Cont@123';
-	public $urlRedirect = "https://contmatic.homologacao.micropower.com.br/Performa/default.aspx";
-	public $urlService = "https://contmatic.homologacao.micropower.com.br/performa/webservice/";
-}
